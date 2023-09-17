dc1cfb62c239be0be3c5ddab6c86783db4344b5b
Natsuki0709 <141482757+Natsuki0709@users.noreply.github.com>
2023-09-17 14:44:04 +0000

    [add]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,10 +6,11 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to '/index'
+    redirect_to '/175343'
   end
   
   def index
+    @books = Book.all 
   end
 
   def show
@@ -17,4 +18,10 @@ class BooksController < ApplicationController
 
   def edit
   end
+  
+ private
+  
+  def book_params
+    params.require(:book).permit(:title, :body)
+ end
 end
