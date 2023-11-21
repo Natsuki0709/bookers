533235956093f980dd35ca709380df21f76bf5a6
Natsuki0709 <141482757+Natsuki0709@users.noreply.github.com>
2023-11-21 00:16:15 +0000

    [add]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,13 +4,18 @@ class BooksController < ApplicationController
   end
   
   def create
-    book = Book.new(book_params)
-    book.save
+    @book = Book.new(book_params)
+    if @book.save
     redirect_to '/175343'
+  else
+    @books = Book.all
+    render :index
   end
   
   def index
-    @books = Book.all 
+    @book = Book.new
+    @books = Book.all
+    
   end
 
   def show
